d018ce1ee4e185a90bdc6fd5d14d19c6e55c3a8e
abdallah <abdallahantony55.aa@gmail.com>
2022-11-08 15:07:41 +0300

    Make insert icon clickable

--- src/index.js
+++ src/index.js
@@ -5,12 +5,17 @@ import clearCompleted from './modules/clearCompletedTodo';
 import registerGraggableEventListeners from './modules/draggableSort';
 
 const createTodoForm = document.querySelector('#todo-form');
+const insertIcon = document.querySelector('.insert-icon');
 const clearCompletedBtn = document.querySelector('.todo-clear');
 
 renderTodos();
 
 clearCompletedBtn.addEventListener('click', () => {
   clearCompleted();
+});
+
+insertIcon.addEventListener('click',()=>{
+  addTodo(createTodoForm);
 });
 
 createTodoForm.addEventListener('submit', (e) => {
